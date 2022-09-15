adcbd886c62e58bc16ee188c45016c9e778ae54e
Jay Schneider <jay.schneider@zweitag.de>
2022-09-15 22:28:48 +0200

    feat: Allow mapping attributes in to/migration

--- spec/infer_model/to/migration_spec.rb
+++ spec/infer_model/to/migration_spec.rb
@@ -96,5 +96,58 @@ RSpec.describe InferModel::To::Migration do
         RUBY
       end
     end
+
+    context "with an attributes map" do
+      subject(:call) { described_class.call(model, attributes_map:, target_dir:) }
+      let(:attributes_map) do
+        {
+          integer_col: :some_unique_present_number,
+          decimal_col: :a_floating_number,
+          datetime_col: :birthday,
+          time_col: :some_time,
+          boolean_col: :truthSpoken?,
+          uuid_col: :unique_uuid,
+        }
+      end
+      let(:model) do
+        InferModel::Model.new(
+          source_name: "happy_path",
+          attributes: {
+            integer_col: InferModel::CommonType.new(:integer, unique_constraint_possible: true, non_null_constraint_possible: true),
+            decimal_col: InferModel::CommonType.new(:decimal),
+            datetime_col: InferModel::CommonType.new(:datetime, non_null_constraint_possible: true),
+            time_col: InferModel::CommonType.new(:time),
+            boolean_col: InferModel::CommonType.new(:boolean, non_null_constraint_possible: true),
+            json_col: InferModel::CommonType.new(:json),
+            uuid_col: InferModel::CommonType.new(:uuid, unique_constraint_possible: true),
+            string_col: InferModel::CommonType.new(:string),
+          },
+        )
+      end
+
+      it "creates a migration file" do
+        expect { call }.to change { File.exist?(expected_filename) }.from(false).to(true)
+        expect(File.read(expected_filename)).to eq(<<~RUBY)
+          # frozen_string_literal: true
+
+          class CreateHappyPaths < ActiveRecord::Migration[7.0]
+            def change
+              create_table "happy_paths" do |t|
+                t.integer "some_unique_present_number", null: false
+                t.decimal "a_floating_number"
+                t.datetime "birthday", null: false
+                t.time "some_time"
+                t.boolean "truthSpoken?", null: false
+                t.uuid "unique_uuid"
+                t.index ["some_unique_present_number"], unique: true
+                t.index ["unique_uuid"], unique: true
+
+                t.timestamps
+              end
+            end
+          end
+        RUBY
+      end
+    end
   end
 end
