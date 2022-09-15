3cf5954613c651a572c9b21287aedb806b29f952
Jay Schneider <jay.schneider@zweitag.de>
2022-09-15 22:51:38 +0200

    feat: allow renaming attributes in from/csv

--- spec/infer_model/from/csv_spec.rb
+++ spec/infer_model/from/csv_spec.rb
@@ -44,5 +44,64 @@ RSpec.describe InferModel::From::CSV do
         )
       end
     end
+
+    context "when rename_attributes is given" do
+      subject(:result) { described_class.call("spec/fixtures/csv/happy_path.csv", rename_attributes:) }
+
+      context "as a hash" do
+        let(:rename_attributes) do
+          {
+            integer_col: :some_unique_present_number,
+            decimal_col: :a_floating_number,
+            datetime_col: :birthday,
+            time_col: :some_time,
+            boolean_col: :truthSpoken?,
+            uuid_col: :unique_uuid,
+          }
+        end
+
+        it "renames the specified keys and keeps the others" do
+          expect(result).to have_attributes(
+            class: InferModel::Model,
+            source_name: "happy_path",
+            attributes: {
+              some_unique_present_number: an_object_having_attributes(detected_type: :integer),
+              a_floating_number: an_object_having_attributes(detected_type: :decimal),
+              birthday: an_object_having_attributes(detected_type: :datetime),
+              some_time: an_object_having_attributes(detected_type: :time),
+              truthSpoken?: an_object_having_attributes(detected_type: :boolean),
+              json_col: an_object_having_attributes(detected_type: :json),
+              unique_uuid: an_object_having_attributes(detected_type: :uuid),
+              string_col: an_object_having_attributes(detected_type: :string),
+            },
+          )
+        end
+      end
+
+      context "as a proc" do
+        let(:rename_attributes) do
+          proc do |old_key|
+            old_key == :string_col ? nil : "some_#{old_key}_with_interesting_properties".to_sym
+          end
+        end
+
+        it "renames with present results and keeps the others" do
+          expect(result).to have_attributes(
+            class: InferModel::Model,
+            source_name: "happy_path",
+            attributes: {
+              some_integer_col_with_interesting_properties: an_object_having_attributes(detected_type: :integer),
+              some_decimal_col_with_interesting_properties: an_object_having_attributes(detected_type: :decimal),
+              some_datetime_col_with_interesting_properties: an_object_having_attributes(detected_type: :datetime),
+              some_time_col_with_interesting_properties: an_object_having_attributes(detected_type: :time),
+              some_boolean_col_with_interesting_properties: an_object_having_attributes(detected_type: :boolean),
+              some_json_col_with_interesting_properties: an_object_having_attributes(detected_type: :json),
+              some_uuid_col_with_interesting_properties: an_object_having_attributes(detected_type: :uuid),
+              string_col: an_object_having_attributes(detected_type: :string),
+            },
+          )
+        end
+      end
+    end
   end
 end
